825fb90aed74cf047b53eb4ef8c5bf5a5e2929eb
praveen-sree <praveen.sree@gmail.com>
2021-05-17 10:24:20 +0530

    Update multiplication_table_for_a_number.c

--- multiplication_table_for_a_number.c
+++ multiplication_table_for_a_number.c
@@ -7,8 +7,17 @@ int main()
     int value1;
     int i;
     printf("Enter a Number   : ");
+    //scanf - read the value and assign to value1
+    // %d - used to accept integer values
     scanf("%d",&value1);
+    // \n used for new line
+    // %d - used to print the integer value of value1
     printf("\n Multiplication Table for %d is \n",value1);
+    // loop for 1-10.
+    // %d -
+    // 1. used to print value1
+    // 2. used to print value of i
+    // 3. used to print (value1 * i)
     for (i=1 ; i<=10;i++)
     {
         printf("\n %d x %d  = %d",value1,i, value1*i);
